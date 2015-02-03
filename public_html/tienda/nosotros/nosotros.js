1a8bdf464d8375f10b8975b42c3f6eaa279a607f
ivanerre <i94773007@gmail.com>
2015-02-03 11:55:07 +0100

    03/02/2015   - Sobre Nosotros   - Versión Español-Inglés (sin BD)

---
+++ public_html/tienda/nosotros/nosotros.js
@@ -0,0 +1,17 @@
+app.controller("DetalleCarritoController", ["$location", "$scope", "$http", "$routeParams", function MainController($location, $scope, $http, $routeParams) {
+
+        var myOptions = {
+            zoom: 17,
+            center: new google.maps.LatLng(39.4806984, -0.4230923000000075),
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+
+        map = new google.maps.Map(document.getElementById("gmap_canvas"), myOptions);
+        marker = new google.maps.Marker({map: map, position: new google.maps.LatLng(39.4806984, -0.4230923000000075)});
+        infowindow = new google.maps.InfoWindow({content: "<b>CyCy Corporation</b><br/>Calle de la piruleta<br/>Pais de la gominola"});
+        google.maps.event.addListener(marker, "click", function () {
+            infowindow.open(map, marker);
+        });
+        infowindow.open(map, marker);
+
+    }]);
