c9bbccde14ac7d06668b14caee8b8d0a5073d216
ivanerre <i94773007@gmail.com>
2015-02-12 10:19:41 +0100

    12/02/2015: Connection switcher.

---
+++ public_html/commons/connections/connectionSwitch-local.bat
@@ -0,0 +1,4 @@
+set sourcedir=C:\wamp\www\CyCyStore\public_html\
+
+cp %sourcedir%commons\connections\localConnection.php %sourcedir%tienda\conexion.php
+cp %sourcedir%commons\connections\localConnection.php %sourcedir%administracion\conexion.php
---
+++ public_html/commons/connections/connectionSwitch-openshit.bat
@@ -0,0 +1,4 @@
+set sourcedir=C:\wamp\www\CyCyStore\public_html\
+
+cp %sourcedir%commons\connections\openshitConnection.php %sourcedir%tienda\conexion.php
+cp %sourcedir%commons\connections\openshitConnection.php %sourcedir%administracion\conexion.php
